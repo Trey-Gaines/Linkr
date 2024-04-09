b36f98accca8fdf3fe7163b779b47d262037d488
Trey <88073284+Trey-Gaines@users.noreply.github.com>
2024-04-09 17:25:23 -0500

    Initial Commit

---
+++ Linkr/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Linkr
+//
+//  Created by Trey Gaines on 4/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Linkr/LinkrApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinkrApp.swift
+//  Linkr
+//
+//  Created by Trey Gaines on 4/9/24.
+//
+
+import SwiftUI
+
+@main
+struct LinkrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
